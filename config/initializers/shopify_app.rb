f0c2dff20cdd76cbb2fd2ccb6d43881228bfae55
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-07-15 15:40:56 -0400

    Update config

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "<api_key>"
   config.secret = "<secret>"
+  config.redirect_uri = "https://rewarding-shopify.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
